87d83755c5d6460ddd272e710b267183d4cfc2b6
Daniel Felix Ferber <X7WS>
2014-06-23 13:20:48 -0300

    [fix] Sync does not update control files.

---
+++ GitToClearCase/src/br/com/danielferber/gittocc2/config/clearcase/ClearToolConfigSourceImpl.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.danielferber.gittocc2.config.clearcase;
+
+import java.io.File;
+
+
+abstract class ClearToolConfigSourceImpl implements ClearToolConfigSource {
+
+    public File getCommitStampAbsoluteFile() {
+        return new File(getVobViewDir(), getCommitStampFileName().getPath());
+    }
+
+    @Override
+    public File getCounterStampAbsoluteFile() {
+        return new File(getVobViewDir(), getCounterStampFileName().getPath());
+    }
+    
+}
